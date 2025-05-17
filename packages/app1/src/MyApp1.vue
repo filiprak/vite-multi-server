e9bd4f676441e21273f1ac2ba753481681119137
Filip Rak <filip.rak@ikol.com>
2025-05-17 17:15:25 +0200

    Cleanup

--- packages/app1/src/MyApp1.vue
+++ packages/app1/src/MyApp1.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
+    <div class="app1">
         my app 1
     </div>
 </template>
 <script setup lang="ts">
+import styles from './styles.css?inline';
+
+console.log(styles)
 
 </script>
--- packages/app2/src/index.ts
+++ packages/app2/src/index.ts
@@ -1,1 +1,1 @@
-console.log('app2')
+document.body.className = 'app2';
