794fe079ce480f50146ce29d2f4b6ff4accc7c7d
Filip Rak <filip.rak@ikol.com>
2025-05-25 18:55:24 +0200

    Cleanup

--- packages/site/vite.config.mjs
+++ packages/site/vite.config.mjs
@@ -3,22 +3,27 @@ import federation from '@repo/compiler/federation-plugin';
 
 export default defineConfig((env) => {
     const is_remote = !!process.env.REMOTE;
+    const is_ssr = !!process.env.SSR;
+
+    console.log('Is remote', is_remote)
+    console.log('Is ssr', is_ssr)
 
     return {
+        ssr: is_ssr,
         build: {
             minify: false,
             cssMinify: false,
             emptyOutDir: !is_remote,
             rollupOptions: {
-                input: is_remote ? './src/remote/app1/index.ts' : './index.html',
+                input: is_remote ? './src/remote/app1/index.ts' : (is_ssr ? './src/index.ts' : './index.html'),
                 output: {
                     hashCharacters: 'base36',
-                    entryFileNames: is_remote ? `remote.js` : `host_[hash:6].js`,
+                    entryFileNames: is_remote ? `remote.js` : `host.js`,
                     chunkFileNames: is_remote ? `remote_[name]_[hash:6].js` : `host_[hash:6].js`,
                     assetFileNames: is_remote ? `remote_[name]_[hash:6].[ext]` : `host_[hash:6].[ext]`,
                 },
             },
-            // target: 'esnext',
+            target: is_ssr ? 'node22' : undefined,
         },
         plugins: [
             federation({
