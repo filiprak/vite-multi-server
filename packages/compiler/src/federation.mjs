8568ac524782ddbf7c938603c652f1112589a92e
Filip Rak <filip.rak@ikol.com>
2025-05-25 16:44:56 +0200

    Cleanup [skip ci]

--- packages/compiler/src/federation.mjs
+++ packages/compiler/src/federation.mjs
@@ -1,9 +1,36 @@
-export function getSharedMap() {
+export function getSharedMap () {
     return '[[_SHARED_MAP_]]';
 }
 
-export async function init() {
-    const f = globalThis.__federation__ = globalThis.__federation__ || {};
+export function getGlobal () {
+    if (!globalThis.__federation__) {
+        globalThis.__federation__ = {};
+    }
+
+    return globalThis.__federation__;
+}
+
+export function init () {
+    const f = getGlobal();
 
     f.shared = getSharedMap();
+    f.remotes = {};
+}
+
+export function addRemote (options) {
+    const f = getGlobal();
+
+    const config = {
+        ...options,
+        load () {
+            return import(config.url);
+        },
+    };
+    f.remotes[options.name] = config;
+}
+
+export async function loadRemote (name) {
+    const f = getGlobal();
+
+    return f.remotes[name].load();
 }
--- packages/site/src/index.ts
+++ packages/site/src/index.ts
@@ -1,7 +1,13 @@
 import { coreFunc } from '@repo/site/core';
-import { init } from 'virtual:federation';
+import { init, addRemote, loadRemote } from 'virtual:federation';
 
 coreFunc();
 init();
+addRemote({
+    name: 'app1',
+    url: '/remote.js',
+});
 
 console.log('./src/index.ts');
+
+loadRemote('app1')
