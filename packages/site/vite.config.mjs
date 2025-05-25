737034814041b3ada235bd4d9e82ec7b27708599
Filip Rak <filip.rak@ikol.com>
2025-05-25 16:37:01 +0200

    Cleanup

--- packages/site/vite.config.mjs
+++ packages/site/vite.config.mjs
@@ -1,25 +1,36 @@
 import { defineConfig } from 'vite';
 import federation from '@repo/compiler/federation-plugin';
 
-export default defineConfig({
-    build: {
-        minify: false,
-        cssMinify: false,
-        rollupOptions: {
-            input: './index.html',
-        },
-        target: 'node22',
-    },
-    plugins: [
-        federation({
-            shared: {
-                '@repo/site/core': {
+export default defineConfig((env) => {
+    const is_remote = !!process.env.REMOTE;
 
-                },
-                '@repo/site/ui': {
-                    
+    return {
+        build: {
+            minify: false,
+            cssMinify: false,
+            emptyOutDir: !is_remote,
+            rollupOptions: {
+                input: is_remote ? './src/remote/app1/index.ts' : './index.html',
+                output: {
+                    hashCharacters: 'base36',
+                    entryFileNames: is_remote ? `remote.js` : `host_[hash:6].js`,
+                    chunkFileNames: is_remote ? `remote_[name]_[hash:6].js` : `host_[hash:6].js`,
+                    assetFileNames: is_remote ? `remote_[name]_[hash:6].[ext]` : `host_[hash:6].[ext]`,
                 },
             },
-        }),
-    ],
+            // target: 'node22',
+        },
+        plugins: [
+            federation({
+                shared: {
+                    '@repo/site/core': {
+                        virtual: is_remote,
+                    },
+                    '@repo/site/ui': {
+                        virtual: is_remote,
+                    },
+                },
+            }),
+        ],
+    };
 });
